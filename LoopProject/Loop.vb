ba87b9fc18b7b9e2b661f5ebc616b882bfaf5b6a
wjackson24 <wjackson@williejackson.info>
2015-08-02 10:08:50 -0700

    adding project

---
+++ LoopProject/Loop.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+    Sub Main()
+        Dim currentNumber = 1
+        Dim lastNumber = 100
+
+        Do While currentNumber <= lastNumber
+            Console.WriteLine(currentNumber)
+            currentNumber = currentNumber + 1
+        Loop
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
